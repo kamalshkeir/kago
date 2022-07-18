bb813e3388dfb8991859d814ce43f1a8b5e90ba5
kamalshkeir <kamalshkeir@yahoo.fr>
2022-07-18 19:24:47 +0200

    fix New not using env files

--- kago.go
+++ kago.go
@@ -7,7 +7,7 @@ import (
 
 func New(env_files ...string) *kamux.Router {
 	// init server and router
-	app := kamux.New()
+	app := kamux.New(env_files...)
 	// init admin urls
 	go admin.UrlPatterns(app)
 	return app
