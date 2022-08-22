dea79dc316f7e034b63803bdad61c82e386da747
kamalshkeir <kamalshkeir@yahoo.fr>
2022-08-22 04:49:15 +0200

    l

--- core/kamux/server.go
+++ core/kamux/server.go
@@ -128,9 +128,15 @@ func (router *Router) RunTLS(certFile string, keyFile string) {
 	if settings.Config.Cert != "" && settings.Config.Key != "" && settings.Proxy {
 		// proxy
 		go func() {
-			http.ListenAndServeTLS(settings.Config.Host+":443",settings.Config.Cert,settings.Config.Key,http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			http.ListenAndServeTLS(settings.Config.Host+":"+settings.Config.Port,settings.Config.Cert,settings.Config.Key,http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 				if utils.StringContains(r.Host,"localhost:9313","127.0.0.1:9313","0.0.0.0:9313") {
-					router.Server.Handler.ServeHTTP(w,r)
+					path,err := url.Parse("http://localhost:9313")
+					if err != nil {
+						fmt.Println(err)
+						return
+					}
+					proxy := reverseproxy.NewReverseProxy(path)
+					proxy.ServeHTTP(w,r)
 				} else {
 					for dom,urll := range proxies {
 						if strings.Contains(r.Host,dom) {
