dc73bdb93483f3aa9d6b59459777af035b1ff0a9
kamalshkeir <kamalshkeir@yahoo.fr>
2022-08-27 22:23:10 +0200

    server tls config update

--- core/kamux/server.go
+++ core/kamux/server.go
@@ -56,6 +56,23 @@ func (router *Router) initServer() {
 		ReadTimeout:  ReadTimeout,
 		WriteTimeout: WriteTimeout,
 		IdleTimeout:  IdleTimeout,
+		TLSConfig: &tls.Config{
+			MinVersion: tls.VersionTLS12,
+			PreferServerCipherSuites: true,
+			CurvePreferences: []tls.CurveID{
+				tls.CurveP256,
+				tls.X25519, 
+			},
+			CipherSuites: []uint16{
+				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305, 
+				tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,   
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			},
+		},
+		
 	}
 	router.Server = &server
 }
